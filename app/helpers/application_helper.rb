f2d7acd920a6073f2983edb3ca9e1549e5fa5114
Nerida Ladner <spacecadetnerro@gmail.com>
2005-12-08 01:58:46 +0000

    created napts stylesheet using divs to improve the layout of the edit page

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,3 +1,7 @@
-# Methods added to this helper will be available to all templates in the application.
 module ApplicationHelper
+  def link_to_button(name, options = {}, html_options = nil, *parameters_for_method_reference)
+    location = url_for(options, *parameters_for_method_reference)
+    options = {"onclick" => "location.href='#{location}'; return false;"}.update(options.stringify_keys)
+    content_tag("button", name, options)
+  end
 end
