7a9df05ebae71272a008ca0423942cf297972a68
Nerida Ladner <spacecadetnerro@gmail.com>
2005-12-08 06:21:04 +0000

    added brackets for methods

--- app/models/answer.rb
+++ app/models/answer.rb
@@ -1,4 +1,4 @@
 class Answer < ActiveRecord::Base
-  validates_presence_of :content
-  belongs_to :question
+  validates_presence_of( :content )
+  belongs_to( :question )
 end
--- app/models/question.rb
+++ app/models/question.rb
@@ -1,4 +1,5 @@
 class Question < ActiveRecord::Base
-  validates_presence_of :content
-  has_many :answers, :dependent => true
+  validates_presence_of( :content )
+  has_many( :answers, :dependent => true )
+  
 end
