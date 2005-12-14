29c9ca16b910cd068c819ac8c37cc39d17cd9e8d
Nerida Ladner <spacecadetnerro@gmail.com>
2005-12-14 08:45:47 +0000

    Rename controller test from "user" to "login"

--- test/functional/login_controller_test.rb
+++ test/functional/login_controller_test.rb
@@ -1,17 +1,16 @@
 require File.dirname(__FILE__) + '/../test_helper'
-require 'user_controller'
+require 'login_controller'
 
 # Re-raise errors caught by the controller.
-class UserController; def rescue_action(e) raise e end; end
+class LoginController; def rescue_action(e) raise e end; end
 
-class UserControllerTest < Test::Unit::TestCase
+class LoginControllerTest < Test::Unit::TestCase
   def setup
-    @controller = UserController.new
+    @controller = LoginController.new
     @request    = ActionController::TestRequest.new
     @response   = ActionController::TestResponse.new
   end
 
-  # Replace this with your real tests.
   def test_truth
     assert true
   end
