0649b866ebb8466c11d5e648d45f892e672e725c
Nerida Ladner <spacecadetnerro@gmail.com>
2005-12-14 08:55:04 +0000

    Empty tests of anything

--- test/unit/quiz_item_test.rb
+++ test/unit/quiz_item_test.rb
@@ -1,10 +1,6 @@
 require File.dirname(__FILE__) + '/../test_helper'
 
 class QuizItemTest < Test::Unit::TestCase
-  fixtures :quiz_items
-
-  # Replace this with your real tests.
   def test_truth
-    assert_kind_of QuizItem, quiz_items(:first)
   end
 end
--- test/unit/quiz_test.rb
+++ test/unit/quiz_test.rb
@@ -1,10 +1,6 @@
 require File.dirname(__FILE__) + '/../test_helper'
 
 class QuizTest < Test::Unit::TestCase
-  fixtures :quizzes
-
-  # Replace this with your real tests.
   def test_truth
-    assert_kind_of Quiz, quizzes(:first)
   end
 end
