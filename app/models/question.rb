f1ea6d98b7899fb60ec5441f3126d1302535afc5
Nerida Ladner <spacecadetnerro@gmail.com>
2005-12-19 06:09:23 +0000

    created validate method in model to ensure that only one answer is_correct if the question is of single andwer type. adjusted list, show and _form to describe qntype rather than display the int it represents

--- app/models/question.rb
+++ app/models/question.rb
@@ -2,4 +2,17 @@ class Question < ActiveRecord::Base
   validates_presence_of( :content )
   has_many( :answers, :dependent => true )
   has_many( :quiz_items )
+  
+  QUESTION_TYPE = {
+    "Multiple answers" => 1,
+    "Single answer" => 2
+  }.freeze
+ 
+  def validate
+    count = 0
+    self.answers.each { |x| count += 1 if x.is_correct? } 
+    if question_type == 2 && count > 1
+      errors.add_to_base( "Must have a single correct answer" )
+    end
+  end
 end
