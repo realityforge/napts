d9ea59d57a8d043e00186d26aa4ec185e772364e
Nerida Ladner <spacecadetnerro@gmail.com>
2005-12-22 05:10:47 +0000

    added more quiz-items to the fixtures from intro can view prelim or start quiz if start quiz is selected each question that is on the quiz is shown. if only one correct answer a flash alert is shown if no answer is selected. when quiz is finished the questions that were incorrect are shown before redirecting to the intro viewing prelim looks the same as the actual test but all questions are shown and no answers are saved

--- app/controllers/quiz_attempt_controller.rb
+++ app/controllers/quiz_attempt_controller.rb
@@ -10,39 +10,86 @@ class QuizAttemptController < ApplicationController
                                         :user_id => @user.id )
 
     for quiz_item in @quiz.quiz_items
-      QuizResponse.create( :created_at => Time.now, 
-                           :question_id => quiz_item.question.id,
-			   :position => quiz_item.position,
-			   :quiz_attempt_id => @quiz_attempt.id )
+      if quiz_item.is_on_test
+        QuizResponse.create( :created_at => Time.now, 
+                             :question_id => quiz_item.question.id,
+	         	     :position => quiz_item.position,
+			     :quiz_attempt_id => @quiz_attempt.id )
+      end
     end
     redirect_to( :action => 'show',
                  :quiz_attempt_id => @quiz_attempt.id,
 		 :quiz_response_position => 1 )
   end
   
-  def show
-    position = params[:quiz_response_position]
-    attempt_id = params[:quiz_attempt_id]
-    @quiz_response = QuizResponse.find( :first, 
-                                   :conditions => [ 'position = ? AND quiz_attempt_id =?', 
-				                    position, 
-				                    attempt_id ] 
-  				 )
-    if request.get? && !@quiz_response
-      redirect_to( :action => 'results', :quiz_attempt_id => attempt_id )
-    elsif request.post?
-      for answer in params[:answers]
-        @quiz_response.answers << Answer.find(answer)
+  def prelim
+    @quiz = Quiz.find(params[:quiz_id])
+    position = params[:quiz_item_position]
+    @quiz_item = QuizItem.find( :first, 
+                                :conditions => ['quiz_id = ? AND position = ?',
+	                        @quiz.id , position ] )
+    if @quiz_item
+      @question = @quiz_item.question
+      if request.post?
+       redirect_to( :action => 'prelim', 
+                    :quiz_item_position => position.to_i + 1,
+		    :quiz_id => @quiz.id )
       end
-      redirect_to( :action => 'show',
-	           :quiz_attempt_id => attempt_id,
-		   :quiz_response_position => position.to_i + 1 )
+    else
+      redirect_to( :action => 'intro' )
     end
   end
   
+  def show
+    position = params[:quiz_response_position]
+    attempt_id = params[:quiz_attempt_id]
+    @quiz_response = 
+        QuizResponse.find( :first, 
+                           :conditions => [ 'position = ? AND quiz_attempt_id =?', 
+	    	                            position, attempt_id ]
+				   )
+    if request.get? && !@quiz_response
+      redirect_to( :action => 'end_quiz', :quiz_attempt_id => attempt_id )
+    elsif request.post?
+      if (( @quiz_response.question.question_type == 2 ) && ( params[:answers] == nil ))
+	flash[:alert] = "Must select an answer"
+      else
+        if ! (params[:answers] == nil)
+          for answer in params[:answers]
+            @quiz_response.answers << Answer.find(answer)
+          end
+	end
+	@quiz_response.update_attributes( :created_at => Time.now )
+        redirect_to( :action => 'show',
+	           :quiz_attempt_id => attempt_id,
+		   :quiz_response_position => position.to_i + 1 )
+      end
+    end
+  end
+  
+  def end_quiz
+    @quiz_attempt =  QuizAttempt.find(params[:quiz_attempt_id])
+    @quiz_attempt.update_attributes( :end_time => Time.now )
+    redirect_to( :action => 'results', :quiz_attempt_id => @quiz_attempt.id )
+  end
+  
   def results
-    
-    @quiz_response = QuizResponse.find( :all, :conditions => ['quiz_attempt_id = ?', :quiz_attempt_id] )
-    
+    @quiz_responses = 
+      QuizResponse.find( :all, :conditions => ['quiz_attempt_id = ?', 
+                                                params[:quiz_attempt_id]] )
+    @results = []
+      for quiz_response in @quiz_responses
+        responses = [] 
+        correct = []
+        for qr in quiz_response.answers
+          responses << qr.answer_id.to_s
+        end
+        for q in quiz_response.question.answers
+          correct << q.id.to_s if q.is_correct
+        end
+        if ! (responses == correct)
+          @results << quiz_response.position.to_s
+        end
+      end
   end
 end
