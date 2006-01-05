b9c93274af9417b1ad2f9747a1f996172b50232e
Nerida Ladner <spacecadetnerro@gmail.com>
2006-01-05 00:32:53 +0000

    html was corrected and brackets added to methods

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,7 +1,9 @@
 module ApplicationHelper
   def link_to_button(name, options = {}, html_options = nil, *parameters_for_method_reference)
     location = url_for(options, *parameters_for_method_reference)
-    options = {"onclick" => "location.href='#{location}'; return false;"}.update(options.stringify_keys)
-    content_tag("button", name, options)
+    html_options = (html_options || {}).stringify_keys
+    html_options['onclick'] = "location.href='#{location}';"
+    html_options['type'] = 'button'
+    content_tag('button', name, html_options)
   end
 end
