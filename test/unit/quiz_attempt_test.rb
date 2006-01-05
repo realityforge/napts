b01391234162994b03b30210da5c314746d19f50
Nerida Ladner <spacecadetnerro@gmail.com>
2006-01-05 07:09:53 +0000

    currently tests the length of the score array returned and unsuccessfully checks which answers are wrong

--- test/unit/quiz_attempt_test.rb
+++ test/unit/quiz_attempt_test.rb
@@ -1,6 +1,16 @@
 require File.dirname(__FILE__) + '/../test_helper'
 
 class QuizAttemptTest < Test::Unit::TestCase
-  def test_truth
+  fixtures OrderedTables
+
+  def test_score_length
+    quiz_attempt = QuizAttempt.find( @qa_2.id )
+    assert_equal( 2, quiz_attempt.score.length )
+  end
+  
+  def test_score_returns_correct_qn_numbers
+    quiz_attempt = QuizAttempt.find( @qa_1.id )
+    assert_equal( 2, quiz_attempt.score.length )
+    assert_equal( 13, answers_quiz_responses(:a1_qr4).answer_id.to_i )
   end
 end
