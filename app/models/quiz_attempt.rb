f8eb0bbbe964b0b523c924cfbda37d042e61aaa7
Nerida Ladner <spacecadetnerro@gmail.com>
2006-01-05 03:21:48 +0000

    a score method was added to the quiz-attempt model that returns an array of the qnnumber of all the incorrect responses for a quiz attempt. the index and total methods and views had been replaced by statistics

--- app/models/quiz_attempt.rb
+++ app/models/quiz_attempt.rb
@@ -2,4 +2,22 @@ class QuizAttempt < ActiveRecord::Base
   belongs_to( :quiz )
   belongs_to( :user )
   has_many( :quiz_responses )
+  
+  def score
+    results = []
+    for quiz_response in self.quiz_responses
+      responses = []
+      correct = []
+      for qr in quiz_response.answers
+        responses << qr.answer_id.to_s
+      end
+      for question in quiz_response.question.answers
+        correct << question.id.to_s if question.is_correct
+      end
+      if ! ( responses == correct )
+        results << quiz_response.position.to_s
+      end
+    end
+    return results
+  end
 end
