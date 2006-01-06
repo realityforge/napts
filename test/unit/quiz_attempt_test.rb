8207f934cea40977ade289c3ebbd71543fe1811a
Nerida Ladner <spacecadetnerro@gmail.com>
2006-01-06 00:29:16 +0000

    there is a test to ensure that if no answer is selected for a multichoice question it is recorded as incorrect. tests to ensure the array of incorrect answers returned matches the incorrect answers

--- test/unit/quiz_attempt_test.rb
+++ test/unit/quiz_attempt_test.rb
@@ -3,14 +3,18 @@ require File.dirname(__FILE__) + '/../test_helper'
 class QuizAttemptTest < Test::Unit::TestCase
   fixtures OrderedTables
 
-  def test_score_length
+  def test_incorrect_answers_multichoice_with_no_answers_selected
     quiz_attempt = QuizAttempt.find( @qa_2.id )
-    assert_equal( 2, quiz_attempt.score.length )
+    assert_equal( 2, quiz_attempt.incorrect_answers.length )
+    assert_equal( 3, quiz_attempt.quiz_responses.count )
+    assert_equal( ["1", "2"], quiz_attempt.incorrect_answers )
   end
   
-  def test_score_returns_correct_qn_numbers
+  def test_incorrect_answers_returns_correct_qn_numbers
     quiz_attempt = QuizAttempt.find( @qa_1.id )
-    assert_equal( 2, quiz_attempt.score.length )
-    assert_equal( 13, answers_quiz_responses(:a1_qr4).answer_id.to_i )
+    quiz_response = QuizResponse.find( @qr_4.id )
+    assert_equal( 2, quiz_attempt.incorrect_answers.length )
+    assert_equal( 13, quiz_response.answers[0].id )
+    assert_equal( ["2", "3"], quiz_attempt.incorrect_answers )
   end
 end
