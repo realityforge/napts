c6e53b435988840de83291ccd7753fc38b50e49e
Nerida Ladner <spacecadetnerro@gmail.com>
2006-01-08 05:04:01 +0000

    adjusted the form to include the staffmember attribute and the controller to redirect to a list of all the users after a new user has been created

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,7 +6,7 @@ class UsersController < ApplicationController
       @user = User.new(params[:user])
       if @user.save
         flash[:notice] = "User #{@user.username} created"
-        redirect_to( :controller => 'subjects', :action => 'list' )
+        redirect_to( :action => 'list' )
       else
         flash[:alert] = "not created"
       end
