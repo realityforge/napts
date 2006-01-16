f480e450b8571aa7c205b2972f288d4d6b1eb32f
Nerida Ladner <spacecadetnerro@gmail.com>
2006-01-16 23:49:48 +0000

    changed enable_quiz to two seperate actions

--- app/controllers/demonstrators/quizzes_controller.rb
+++ app/controllers/demonstrators/quizzes_controller.rb
@@ -18,18 +18,35 @@ class Demonstrators::QuizzesController < Demonstrators::BaseController
 				      :per_page => 10 )
   end
   
+#  def enable
+#    @quiz = Quiz.find(params[:id])
+#    if @quiz.enable
+#      if ! @quiz.update_attributes( :enable => false )
+#        flash[:alert] = "Test could not be disabled"
+#      end
+#    else
+#      if ! @quiz.update_attributes( :enable => true )
+#        flash[:alert] = "Test could not be enabled"
+#      else
+#        flash[:notice] = "Test #{@quiz.name} enabled at #{Time.now.strftime("%H:%M")}"
+#      end
+#    end
+#    redirect_to( :action => 'enable_quiz', :id => @quiz.id )
+#  end
+#  
+  def disable
+    update_quiz( false )
+  end
+  
   def enable
+    update_quiz( true )
+  end
+  
+private
+  def update_quiz(value)
     @quiz = Quiz.find(params[:id])
-    if @quiz.enable
-      if ! @quiz.update_attributes( :enable => false )
-        flash[:alert] = "Test could not be disabled"
-      end
-    else
-      if ! @quiz.update_attributes( :enable => true )
-        flash[:alert] = "Test could not be enabled"
-      else
-        flash[:notice] = "Test #{@quiz.name} enabled at #{Time.now.strftime("%H:%M")}"
-      end
+    if ! @quiz.update_attributes( :enable => value )
+      flash[:alert] = "Failed to update quiz status."
     end
     redirect_to( :action => 'enable_quiz', :id => @quiz.id )
   end
