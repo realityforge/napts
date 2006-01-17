c7b9c120d83e12bf0acb7496746cef77207b6441
Nerida Ladner <spacecadetnerro@gmail.com>
2006-01-17 10:09:27 +0000

    made quiz_attempt dependent on quizzes so subjects can be deleted.

--- app/models/quiz.rb
+++ app/models/quiz.rb
@@ -1,6 +1,6 @@
 class Quiz < ActiveRecord::Base
   has_many( :quiz_items, :dependent => true )
-  has_many( :quiz_attempts )
+  has_many( :quiz_attempts, :dependent => true )
   belongs_to( :subject )
   validates_presence_of( :subject_id )
   validates_numericality_of( :duration )
