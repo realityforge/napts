77970d35cdc78b5ea269b37caafd751032652079
Nerida Ladner <spacecadetnerro@gmail.com>
2006-01-18 03:00:02 +0000

    merged edit and update actions. Removed enable prelim and choice of subjects from _form. Use current_subject in controller

--- app/controllers/teachers/quizzes_controller.rb
+++ app/controllers/teachers/quizzes_controller.rb
@@ -1,35 +1,24 @@
 class Teachers::QuizzesController < Teachers::BaseController
-
   def list
-    verify_teacher
     @quiz_pages, @quizzes = paginate( :quizzes,
-                                      :select => 'quizzes.*',
-                                      :joins => ', teachers',
-                                      :conditions => ['quizzes.subject_id = teachers.subject_id AND teachers.user_id = ?', current_user.id],
+                                      :conditions => ['subject_id = ?', current_subject.id],
                                       :per_page => 10 )
   end
   
   def disable_preview
-    verify_teacher
     update_preview(false)
   end
   
   def enable_preview
-    verify_teacher
     update_preview(true)
   end
   
   def show
-    @quiz = Quiz.find(params[:id])
-    verify_teacher
-    @questions = Question.find(:all,
-                             :conditions => ['quiz_items.quiz_id = ?', @quiz.id],
-			     :joins => 'LEFT OUTER JOIN quiz_items ON quiz_items.question_id = questions.id ')
+    @quiz = current_subject.quizzes.find(params[:id])
   end
   
   def change
-    @quiz = Quiz.find(params[:id])
-    verify_teacher
+    @quiz = current_subject.quizzes.find(params[:id])
     if params[:quiz_item_ids] == nil
       flash[:alert] = 'must select something'
     else
@@ -44,7 +33,7 @@ class Teachers::QuizzesController < Teachers::BaseController
           value = 'false'
         end
         for quiz_item_id in params[:quiz_item_ids]
-          quiz_item = QuizItem.find(quiz_item_id)
+          quiz_item = @quiz.quiz_items.find(quiz_item_id)
           if ! quiz_item.update_attributes( :is_on_test => value )
             flash[:alert] = "not updated"
           end
@@ -56,10 +45,9 @@ class Teachers::QuizzesController < Teachers::BaseController
   
   def new
     @quiz = Quiz.new
-    verify_teacher
-    @subjects = Subject.find(:all)
     if request.post?
     @quiz = Quiz.new( params[:quiz] )
+    @quiz.subject_id = current_subject.id
       if ! @quiz.save
         flash[:alert] = "Quiz could not be created"
       else
@@ -70,42 +58,33 @@ class Teachers::QuizzesController < Teachers::BaseController
   end
   
   def edit
-    @quiz = Quiz.find(params[:id])
-    verify_teacher
-    @subjects = Subject.find(:all)
-  end
-  
-  def update
-    @quiz = Quiz.find(params[:id])
-    verify_teacher
-    if @quiz.update_attributes(params[:quiz])
-      flash[:notice] = 'Quiz was successfully updated.'
-      redirect_to( :action => 'show', :id => @quiz.id )
-    else
-      render( :action => 'edit' )
+    @quiz = current_subject.quizzes.find(params[:id])
+    if request.post?
+      if @quiz.update_attributes(params[:quiz])
+        flash[:notice] = 'Quiz was successfully updated.'
+        redirect_to( :action => 'show', :id => @quiz.id )
+      end
     end
   end
   
   def destroy
-    @quiz = Quiz.find(params[:id])
-    verify_teacher
-    @subject = @quiz.subject_id
+    @quiz = current_subject.quizzes.find(params[:id])
     @quiz.destroy
-    redirect_to( :action => 'list', :subject_id => @subject )
+    redirect_to( :action => 'list' )
   end
   
   def add_questions
-    @quiz = Quiz.find(params[:id])
-    verify_teacher
+    @quiz = current_subject.quizzes.find(params[:id])
     if request.get?
       @questions = Question.find(:all,
-        	         :conditions => ['id NOT IN (SELECT questions.id FROM questions RIGHT OUTER JOIN quiz_items ON quiz_items.question_id = questions.id WHERE quiz_items.quiz_id = ?)', @quiz.id] )
+        	                :conditions => ['id NOT IN (SELECT questions.id FROM questions RIGHT OUTER JOIN quiz_items ON quiz_items.question_id = questions.id WHERE quiz_items.quiz_id = ?) AND subject_group_id = ?',
+				@quiz.id, current_subject.subject_group_id ] )
     else
       if params[:question_ids].nil?
         flash[:alert] = "Must select something"
       else
         for question in params[:question_ids]
-          quiz_item = QuizItem.create( {:quiz_id => params[:id], :question_id => question } )
+          quiz_item = @quiz.quiz_items.create( {:quiz_id => params[:id], :question_id => question } )
 	  if ! quiz_item.valid?
 	    flash[:alert] = "Some questions not added as they were already in quiz"
 	  end
@@ -115,15 +94,10 @@ class Teachers::QuizzesController < Teachers::BaseController
       return
     end
   end
-
-#protected
-#  def current_subject_id
-#    @quiz ? @quiz.subject_id : nil 
-#  end
   
 private 
   def update_preview(value)
-    @quiz = Quiz.find(params[:id])
+    @quiz = current_subject.quizzes.find(params[:id])
     if ! @quiz.update_attributes( :prelim_enable => value )
       flash[:alert] = "Failed to update quiz."
     end
--- lib/subject_system.rb
+++ lib/subject_system.rb
@@ -7,5 +7,6 @@ protected
 
   def current_subject
     @current_subject = Subject.find(current_subject_id) unless @current_subject
+    @current_subject
   end
 end
