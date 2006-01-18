f2a1ba8ee1fa65e0bc46c4933925f4eae893a15f
Peter Donald <peter@realityforge.org>
2006-01-18 23:37:00 +0000

    explicit security checks no longer required

--- app/controllers/demonstrators/quizzes_controller.rb
+++ app/controllers/demonstrators/quizzes_controller.rb
@@ -1,7 +1,6 @@
 class Demonstrators::QuizzesController < Demonstrators::BaseController
   def restart
     @quizzes = current_subject.quizzes.find( :all, :conditions => ['enable = ? AND subject_id = ?', true, session[:subject_id]] )
-    verify_demonstrator
     if request.post?
       if ! @user = User.find( :first, :conditions => ['username = ?', params[:username]] )
         flash[:alert] = "Username invalid"
@@ -23,7 +22,6 @@ class Demonstrators::QuizzesController < Demonstrators::BaseController
 				      :joins => ', demonstrators',
 				      :conditions => ['quizzes.subject_id = demonstrators.subject_id AND demonstrators.user_id = ?', current_user.id],
 				      :per_page => 10 )
-    verify_demonstrator
   end
 
   def disable
@@ -37,7 +35,6 @@ class Demonstrators::QuizzesController < Demonstrators::BaseController
 private
   def update_quiz(value)
     @quiz = current_subject.quizzes.find(params[:id])
-    verify_demonstrator
     if ! @quiz.update_attributes( :enable => value )
       flash[:alert] = "Failed to update quiz status."
     elsif @quiz.enable
