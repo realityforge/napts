d614b195abe3c155566e797a2a5b860eed4a4f9f
Nerida Ladner <spacecadetnerro@gmail.com>
2006-01-19 03:07:04 +0000

    added fixtures for computers

--- app/models/computer.rb
+++ app/models/computer.rb
@@ -1,4 +1,6 @@
 class Computer < ActiveRecord::Base
+  #need to write regular expression for validating ip address...
+  #this one's just temporary
   validates_length_of( :ip_address, :maximum => 16 )
   belongs_to( :room )
   validates_associated( :room )
