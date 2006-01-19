9717d551e4dae8cb30a3f584c8592f407bf8770f
Peter Donald <peter@realityforge.org>
2006-01-19 04:26:33 +0000

    Unit test new controller

---
+++ test/functional/demonstrators/quiz_attempt_controller_test.rb
@@ -0,0 +1,42 @@
+require File.dirname(__FILE__) + '/../../test_helper'
+require 'demonstrators/quiz_attempt_controller'
+
+#Re-raise errors caught by the controller.
+class Demonstrators::QuizAttemptController; def rescue_action(e) raise e end; end
+
+require 'quiz_attempt'
+
+class Quizzes < ActiveRecord::Base
+  cattr_accessor :time
+  def now; @@time; end
+end
+
+class QuizAttemptControllerTest < Test::Unit::TestCase
+  fixtures OrderedTables
+  
+  def setup
+    @controller = Demonstrators::QuizAttemptController.new
+    @request = ActionController::TestRequest.new
+    @response = ActionController::TestResponse.new
+  end
+  
+  def test_list
+    get(:list, 
+        {:quiz_id => @quiz_3.id}, 
+        {:user_id => @mr_fancy_pants_user.id, :role => :demonstrator, :subject_id => @subject_2.id} )
+    assert_response(:success)
+    assert_template('list')
+    assert_valid_markup
+    assert_not_nil(assigns(:quiz_attempts))
+    assert_nil(flash[:alert])
+    assert_nil(flash[:notice])
+  end
+  
+  def test_reset
+    post(:reset, 
+        {:quiz_id => @quiz_3.id, :quiz_attempt_id => @qa_1.id}, 
+        {:user_id => @mr_fancy_pants_user.id, :role => :demonstrator, :subject_id => @subject_2.id} )
+    assert_redirected_to(:action => 'list', :quiz_id => @quiz_3.id)
+    assert_raise(ActiveRecord::RecordNotFound){QuizAttempt.find(@qa_1.id)}
+  end
+end
