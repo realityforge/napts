dbc098a426532bc142353e8abc00eecdcc42377d
Peter Donald <peter@realityforge.org>
2006-01-22 07:46:13 +0000

    Remove debug output

--- test/unit/computer_test.rb
+++ test/unit/computer_test.rb
@@ -14,7 +14,6 @@ class ComputerTest < Test::Unit::TestCase
   
   def test_validate_format_incorrect
     computer = Computer.create( :room_id => 1, :ip_address => "123.458" )
-    STDERR.puts "computer= #{computer.to_yaml}"
     assert_equal( false, computer.valid? )
   end
 end
