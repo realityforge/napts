968edee28d80106250c2aad4f16a7b0035203b07
Peter Donald <peter@realityforge.org>
2006-01-22 07:04:37 +0000

    Rework so that validation of addresses occurs in validate and saving occurs in after_save

--- app/models/room.rb
+++ app/models/room.rb
@@ -1,24 +1,41 @@
 class Room < ActiveRecord::Base
   validates_length_of( :name, :within => 1..50 )
   validates_uniqueness_of( :name )
-  has_many( :computers, :dependent => true )
+  has_many( :computers, :exclusively_dependent => true, :order => 'ip_address' )
   has_and_belongs_to_many( :quizzes )
 
   def addresses
-    self.computers.collect {|c| c.ip_address}.join("\n")
+    @addresses = self.computers.collect {|c| c.ip_address}.join("\n") unless @addresses
+    @addresses
   end
 
   def addresses=(value)
-    addys = value.chomp.split(/\s/).uniq
-    invalid = []
-    addys.each do |address|
-      invalid << address unless (address =~ Computer::AddressRegex)
-    end
-    if invalid.length > 0
-      self.errors.add_to_base("The following ip addresses are badly formatted: #{invalid.join(' ')}")
-    else
+    @addresses = value
+  end
+
+  def validate
+   if @addresses
+     addys = gen_address_list
+     invalid = []
+     addys.each do |address|
+        invalid << address unless (address =~ Computer::AddressRegex)
+      end
+     if invalid.length > 0
+       self.errors.add_to_base("The following ip addresses are badly formatted: #{invalid.join(' ')}")
+     end
+   end
+  end
+
+  def after_save
+    if @addresses
       self.computers.clear
-      addys.each {|address| self.computers.create!(:ip_address => address)}
+      gen_address_list.each {|address| self.computers.create!(:ip_address => address)}
     end
+  end
+
+  private
+
+  def gen_address_list
+    @addresses.gsub(/\s+/,' ').split(/\s/).uniq
   end
 end
