d61e76f4579bea6f0633e8a6b44b6edd5b129268
Peter Donald <peter@realityforge.org>
2006-01-22 00:15:21 +0000

    Continue to rework the subject controller so that it conforms to the flow described in the diagram

--- app/controllers/admins/subject_controller.rb
+++ app/controllers/admins/subject_controller.rb
@@ -1,4 +1,6 @@
 class Admins::SubjectController < Admins::BaseController
+  verify :method => :post, :only => %w( destroy add_teacher remove_teacher )
+  verify :method => :get, :only => %w( show list teachers )
 
   def list
     @subjects = Subject.find(:all)
@@ -10,25 +12,61 @@ class Admins::SubjectController < Admins::BaseController
     if request.post?
       if @subject.save
         flash[:notice] = 'Subject was successfully created.'
-        redirect_to( :action => 'list' )
+        redirect_to(:action => 'show', :id => @subject)
       end
     end
   end
   
+  def show
+    @subject = Subject.find(params[:id])
+  end
+
   def edit
     @subject = Subject.find(params[:id])
     @groups = SubjectGroup.find_all_sorted
     if request.post?
       if @subject.update_attributes(params[:subject])
         flash[:notice] = 'Subject was successfully updated.'
-        redirect_to( :action => 'list' )
+        redirect_to(:action => 'show', :id => @subject)
       end
     end
+  end
+
+  def teachers
+    @subject = Subject.find(params[:id])
+    if params[:q]
+      conditions = 
+        ['id NOT IN (SELECT user_id FROM teachers WHERE subject_id = ?) AND users.username LIKE ?', 
+        @subject.id, 
+        "%#{params[:q]}%"]
+    else
+      conditions = ['id NOT IN (SELECT user_id FROM teachers WHERE subject_id = ?)', @subject.id]
+    end
+    @user_pages, @users = paginate( :users, 
+                                    :conditions => conditions,
+                                    :order_by => 'username',
+                                    :per_page => 20 )
+  end
+
+  def add_teacher
+    @subject = Subject.find(params[:id])
+    @user = User.find(params[:user_id])
+    @subject.teachers << @user
+    flash[:notice] = 'Teacher was successfully added to Subject.'
+    redirect_to(:action => 'teachers', :id => @subject, :q => params[:q], :page => params[:page])
+  end
+
+  def remove_teacher
+    @subject = Subject.find(params[:id])
+    @user = User.find(params[:user_id])
+    @subject.teachers.delete(@user)
+    flash[:notice] = 'Teacher was successfully removed from Subject.'
+    redirect_to(:action => 'show', :id => @subject)
   end
 
   def destroy
     Subject.find(params[:id]).destroy
     flash[:notice] = 'Subject was successfully deleted.'
-    redirect_to( :action => 'list' )
+    redirect_to(:action => 'list')
   end
 end
--- test/functional/admins/subject_controller_test.rb
+++ test/functional/admins/subject_controller_test.rb
@@ -56,11 +56,23 @@ class SubjectControllerTest < Test::Unit::TestCase
     post(:new, 
         {:subject => {:code => 'X', :subject_group_id => @sg_1.id}}, 
         {:user_id => @admin_user.id, :role => :administrator} )
-    assert_redirected_to(:action => 'list')
     assert_not_nil(assigns(:subject))
+    assert_redirected_to(:action => 'show', :id => assigns(:subject).id)
     assert_nil(assigns(:subject).errors[:code])
     assert_nil(flash[:alert])
     assert_equal('Subject was successfully created.',flash[:notice])
+  end
+
+  def test_show
+    get(:show, 
+        {:id => @subject_1.id}, 
+        {:user_id => @admin_user.id, :role => :administrator} )
+    assert_response(:success)
+    assert_template('show')
+    assert_valid_markup
+    assert_not_nil(assigns(:subject))
+    assert_nil(flash[:alert])
+    assert_nil(flash[:notice])
   end
   
   def test_edit_get
@@ -94,12 +106,62 @@ class SubjectControllerTest < Test::Unit::TestCase
     post(:edit, 
         {:id => @subject_1.id, :subject => {:code => 'X', :subject_group_id => @sg_1.id}}, 
         {:user_id => @admin_user.id, :role => :administrator} )
-    assert_redirected_to(:action => 'list')
     assert_not_nil(assigns(:subject))
+    assert_redirected_to(:action => 'show', :id => @subject_1.id)
     assert_equal(@subject_1.id,assigns(:subject).id)
     assert_nil(assigns(:subject).errors[:code])
     assert_nil(flash[:alert])
     assert_equal('Subject was successfully updated.',flash[:notice])
+  end
+
+  def test_teachers
+    get(:teachers, 
+        {:id => @subject_1.id}, 
+        {:user_id => @admin_user.id, :role => :administrator} )
+    assert_response(:success)
+    assert_template('teachers')
+    assert_valid_markup
+    assert_not_nil(assigns(:subject))
+    assert_equal(@subject_1.id,assigns(:subject).id)
+    assert_not_nil(assigns(:users))
+    assert_equal(4,assigns(:users).length)
+    assert_equal(@admin_user.id,assigns(:users)[0].id)
+    assert_equal(@mr_fancy_pants_user.id,assigns(:users)[1].id)
+    assert_equal(@peter_user.id,assigns(:users)[2].id)
+    assert_equal(@sleepy_user.id,assigns(:users)[3].id)
+    assert_not_nil(assigns(:user_pages))
+    assert_nil(flash[:alert])
+    assert_nil(flash[:notice])
+  end
+
+  def test_add_teacher
+    assert_raise(ActiveRecord::RecordNotFound){@subject_1.teachers.find(@peter_user.id)}
+    post(:add_teacher, 
+        {:id => @subject_1.id, :user_id => @peter_user.id, :q => 's', :page => '1'}, 
+        {:user_id => @admin_user.id, :role => :administrator} )
+    assert_redirected_to(:action => 'teachers', :id => @subject_1.id, :q => 's', :page => '1')
+    assert_not_nil(assigns(:subject))
+    assert_equal(@subject_1.id,assigns(:subject).id)
+    assert_not_nil(assigns(:user))
+    assert_equal(@peter_user.id,assigns(:user).id)
+    assert_nil(flash[:alert])
+    assert_equal('Teacher was successfully added to Subject.',flash[:notice])
+    assert_not_nil(@subject_1.teachers.find(@peter_user.id))
+  end
+
+  def test_remove_teacher
+    assert_not_nil(@subject_1.teachers.find(@lecturer_user.id))
+    post(:remove_teacher, 
+        {:id => @subject_1.id, :user_id => @lecturer_user.id}, 
+        {:user_id => @admin_user.id, :role => :administrator} )
+    assert_redirected_to(:action => 'show', :id => @subject_1.id)
+    assert_not_nil(assigns(:subject))
+    assert_equal(@subject_1.id,assigns(:subject).id)
+    assert_not_nil(assigns(:user))
+    assert_equal(@lecturer_user.id,assigns(:user).id)
+    assert_nil(flash[:alert])
+    assert_equal('Teacher was successfully removed from Subject.',flash[:notice])
+    assert_raise(ActiveRecord::RecordNotFound){@subject_1.teachers.find(@lecturer_user.id)}
   end
 
   def test_destroy
