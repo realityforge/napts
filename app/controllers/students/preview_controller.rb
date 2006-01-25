023d1219954e4ecb3758a138c03ae1894ed3c6b4
Peter Donald <peter@realityforge.org>
2006-01-25 02:46:39 +0000

    Rework the flow so that it more closely matches the design

--- app/controllers/students/preview_controller.rb
+++ app/controllers/students/preview_controller.rb
@@ -4,21 +4,13 @@ class Students::PreviewController < Students::BaseController
     @quizzes = Quiz.find( :all, :conditions => ['prelim_enable = ? AND subject_id = ?', true, @subject.id] )
   end
 
+  def show
+    @quiz = Quiz.find(params[:id])
+  end
+
   def view_quiz
-    @quiz = Quiz.find( params[:quiz_id] )
-    position = params[:quiz_item_position]
-    @quiz_item = QuizItem.find( :first,
-                                :conditions => ['quiz_id = ? AND position = ?',
-				                @quiz.id, position ] )
-    if @quiz_item
-      @question = @quiz_item.question
-      if request.post?
-        redirect_to( :action => 'view_quiz', 
-                     :quiz_item_position => position.to_i + 1,
-	       	     :quiz_id => @quiz.id )
-      end
-    else
-      redirect_to(:action => 'list', :subject_id => @quiz.subject_id)
-    end
+    @quiz_item = QuizItem.find(:first,
+                               :conditions => ['quiz_id = ? AND position = ?', params[:id], params[:position] ] )
+    raise ActiveRecord::RecordNotFound, "Couldn't find QuizItem with quiz_id = #{params[:id]} AND position = #{params[:position]}" unless @quiz_item
   end
 end
