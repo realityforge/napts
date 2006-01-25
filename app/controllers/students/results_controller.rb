69782fdc876131875e744baf0f0841105a710bfd
Nerida Ladner <spacecadetnerro@gmail.com>
2006-01-25 03:37:55 +0000

    results are now only listed for one subject at time. the statistics view was changed to list and the results view was changed to show

--- app/controllers/students/results_controller.rb
+++ app/controllers/students/results_controller.rb
@@ -1,18 +1,21 @@
 class Students::ResultsController < Students::BaseController
-  def results
+  def show
     @quiz_attempt = QuizAttempt.find(params[:quiz_attempt])
   end
 
-  def statistics
+  def list
+    @quiz_attempts = QuizAttempt.find(:all,
+                                      :select => 'quiz_attempts.*',
+                                      :joins => 'LEFT OUTER JOIN quizzes ON quizzes.id = quiz_attempts.quiz_id',
+                                      :conditions => ['subject_id = ? AND user_id = ?', params[:subject_id], current_user.id],
+				      :order => 'start_time DESC')
     @quiz_name = []
     @score = []
-    @subject = []
     @date = []
-    for attempt in current_user.quiz_attempts
+    for attempt in @quiz_attempts
       @quiz_name << attempt.quiz.name
       @numqns = attempt.quiz_responses.length
       @score << @numqns - attempt.incorrect_answers.length
-      @subject << attempt.quiz.subject.name
       @date << attempt.start_time
     end
   end
