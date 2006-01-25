2c67302ca53733150f51cbad526e9e72d26603fe
Peter Donald <peter@realityforge.org>
2006-01-25 00:30:23 +0000

    Update menu for students

--- app/controllers/students/base_controller.rb
+++ app/controllers/students/base_controller.rb
@@ -1,6 +1,24 @@
 class Students::BaseController < ApplicationController
   before_filter :verify_student
 
+  helper_method :get_navigation_links
+
+protected
+  def get_navigation_links
+    links = []
+    is_selected = controller_name == 'subject' && @action_name == 'list'
+    links << MenuHelper::Link.new('Subjects',
+                                  {:controller => '/students/subject', :action => 'list'},
+                                  {:title => 'Browse Subjects'},
+                                  {:selected => is_selected}).freeze
+    is_selected = controller_name == 'quiz_attempt' && @action_name == 'list'
+    links << MenuHelper::Link.new('Active Quizzes',
+                                  {:controller => '/students/quiz_attempt', :action => 'list'},
+                                  {:title => 'Browse Quizzes currently active.'},
+                                  {:selected => is_selected}).freeze
+    links
+  end
+
 private
   def verify_student
     raise Napts::SecurityError unless session[:role] == :student
--- app/helpers/menu_helper.rb
+++ app/helpers/menu_helper.rb
@@ -2,23 +2,6 @@ module MenuHelper
   if ! const_defined?(:Link)
     Link = Struct.new( "Link", :name, :link_options, :html_options, :options )
 
-    HomeLink = Link.new('Home',
-      {:controller => 'welcome', :action => 'index'},
-      {:title => 'Return to Home page'},
-      {}).freeze
-    PastQuizzesLink = Link.new('Results',
-      {:controller => '/students/results', :action => 'statistics'},
-      {:title => 'Results for completed OnLine Tests'},
-      {}).freeze
-    CurrentQuizzesLink = Link.new('Take Test',
-      {:controller => '/students/quiz_attempt', :action => 'intro'},
-      {:title => 'Sit enabled online test'},
-      {}).freeze
-    PreviewQuizzesLink = Link.new('Preview Tests',
-      {:controller => '/students/preview_quiz', :action => 'intro'},
-      {:title => 'Preview tests'},
-      {}).freeze
-
     ManageQuizzesLink = Link.new('Manage Quizzes',
       {:controller => '/teachers/quizzes', :action => 'list'},
       {:title => 'Create new quizzes or add and remove questions from existing quizzes'},
@@ -96,12 +79,7 @@ module MenuHelper
 
   def get_navigation_links
     links = []
-    if session[:role] == :student
-      links << HomeLink
-      links << gen_past_quizzes_link.freeze
-      links << gen_current_quizzes_link.freeze
-      links << gen_preview_quizzes_link.freeze
-    elsif session[:role] == :teacher
+    if session[:role] == :teacher
       links << gen_manage_quizzes_link.freeze
       links << gen_manage_questions_link.freeze
     end
@@ -116,21 +94,6 @@ module MenuHelper
     result = link.dup
     result.options.update(:selected => is_selected )
     result
-  end
-
-  def gen_past_quizzes_link
-    is_selected = get_controller_name == '/students/results' && @action_name == 'statistics'
-    dup_link_with_select( PastQuizzesLink, is_selected )
-  end
-
-  def gen_current_quizzes_link
-    is_selected = get_controller_name == '/students/quiz_attempt' && @action_name == 'intro'
-    dup_link_with_select( CurrentQuizzesLink, is_selected )
-  end
-
-  def gen_preview_quizzes_link
-    is_selected = get_controller_name == '/students/preview_quiz' && @action_name == 'intro'
-    dup_link_with_select( PreviewQuizzesLink, is_selected )
   end
 
   def gen_manage_quizzes_link
