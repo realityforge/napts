69a88e2eabee90fa3cfd98bd7b60d1fa851a3339
Peter Donald <peter@realityforge.org>
2006-01-25 04:11:16 +0000

    Retrieve quizzes through the quizzes relationsip

--- app/controllers/students/preview_controller.rb
+++ app/controllers/students/preview_controller.rb
@@ -2,7 +2,7 @@ class Students::PreviewController < Students::BaseController
   verify :method => :get, :only => %w( show list show_question )
   def list
     @subject = Subject.find(params[:subject_id])
-    @quizzes = Quiz.find( :all, :conditions => ['prelim_enable = ? AND subject_id = ?', true, @subject.id] )
+    @quizzes = @subject.quizzes.find( :all, :conditions => ['prelim_enable = ?', true] )
   end
 
   def show
