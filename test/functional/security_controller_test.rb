0a3343f6ab79547b5f8e355e58069f5b0ff01bc9
Peter Donald <peter@realityforge.org>
2006-01-30 02:23:11 +0000

    Unit test the security controller

---
+++ test/functional/security_controller_test.rb
@@ -0,0 +1,24 @@
+require File.dirname(__FILE__) + '/../test_helper'
+require 'security_controller'
+
+# Re-raise errors caught by the controller.
+class SecurityController; def rescue_action(e) raise e end; end
+
+class SecurityControllerTest < Test::Unit::TestCase
+  fixtures OrderedTables
+  
+  def setup
+    @controller = SecurityController.new
+    @request    = ActionController::TestRequest.new
+    @response   = ActionController::TestResponse.new
+  end
+
+  def test_access_denied_get
+    get(:access_denied, {}, {:user_id => users(:peter_user).id, :role => :student})
+    assert_response(:success)
+    assert_template('access_denied')
+    assert_valid_markup
+    assert_nil(flash[:alert])
+    assert_nil(flash[:notice])
+  end
+end
