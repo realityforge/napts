6532801a6251b1551661ec13adec53f54c9abdd8
Peter Donald <peter@realityforge.org>
2006-01-31 02:08:58 +0000

    Test the relationship

--- test/unit/quiz_response_test.rb
+++ test/unit/quiz_response_test.rb
@@ -1,6 +1,11 @@
 require File.dirname(__FILE__) + '/../test_helper'
 
 class QuizResponseTest < Test::Unit::TestCase
-  def test_truth
+  fixtures OrderedTables
+
+  def test_answers
+    quiz_response = QuizResponse.find(quiz_responses(:qr_1).id)
+    assert_equal(1, quiz_response.answers.size)
+    assert_equal(answers(:q4_a1).id, quiz_response.answers[0].id)
   end
 end
