5876e7d3c578f699f71e1c572618ad973be7ea9c
Nerida Ladner <spacecadetnerro@gmail.com>
2006-02-01 05:33:20 +0000

    fixed cia issues

--- app/models/subject_group.rb
+++ app/models/subject_group.rb
@@ -1,6 +1,7 @@
 class SubjectGroup < ActiveRecord::Base
   has_many( :subjects, :dependent => true )
   has_many( :questions, :dependent => true )
+  has_many( :resources, :dependent => true )
   validates_length_of( :name, :within => 1..50 )
   validates_uniqueness_of( :name )
 
