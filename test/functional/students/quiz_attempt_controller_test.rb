d29d819b38817c238d17d5bad3095a676077e8dd
Peter Donald <peter@realityforge.org>
2006-02-01 01:18:27 +0000

    Start to add tests for quiz_attempt controller

---
+++ test/functional/students/quiz_attempt_controller_test.rb
@@ -0,0 +1,30 @@
+require File.dirname(__FILE__) + '/../../test_helper'
+require 'students/quiz_attempt_controller'
+
+#Re-raise errors caught by the controller.
+class Students::QuizAttemptController; def rescue_action(e) raise e end; end
+
+class Students::QuizAttemptControllerTest < Test::Unit::TestCase
+  fixtures OrderedTables
+
+  def setup
+    @controller = Students::QuizAttemptController.new
+    @request = ActionController::TestRequest.new
+    @response = ActionController::TestResponse.new
+  end
+
+  def test_list
+    get(:list,
+        {},
+        {:user_id => users(:sleepy_user).id, :role => :student} )
+    assert_response(:success)
+    assert_template('list')
+    assert_valid_markup
+    assert_not_nil(assigns(:quizzes))
+    assert_equal(1,assigns(:quizzes).length)
+    assert_equal(quizzes(:quiz_1).id,assigns(:quizzes)[0].id)
+    assert_nil(flash[:alert])
+    assert_nil(flash[:notice])
+  end
+
+end
