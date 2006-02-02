6b36bf0526af73d57b90902a20df099eb9b36bc0
Peter Donald <peter@realityforge.org>
2006-02-02 00:37:01 +0000

    Decruft

--- app/models/quiz_response.rb
+++ app/models/quiz_response.rb
@@ -2,8 +2,6 @@ class QuizResponse < ActiveRecord::Base
   belongs_to( :quiz_attempt )
   belongs_to( :question )
   has_and_belongs_to_many( :answers )
-  acts_as_list( :scope => :quiz_attempt )
-  #validates_length_of( :input, :within => 0..125 )
   validates_associated( :quiz_attempt, :question )
   validates_presence_of( :quiz_attempt_id, :question_id )
 end
