c9cf7dc02369e578d3b21e4f09db7ae4eaebc286
Peter Donald <peter@realityforge.org>
2006-02-02 00:15:25 +0000

    Unit test subject group

--- test/unit/subject_group_test.rb
+++ test/unit/subject_group_test.rb
@@ -10,4 +10,24 @@ class SubjectGroupTest < Test::Unit::TestCase
     assert_equal(subject_groups(:sg_1).id,groups[1].id)
     assert_equal(subject_groups(:sg_2).id,groups[2].id)
   end
+
+  def test_subjects
+    group = SubjectGroup.find( subject_groups(:sg_1).id )
+    assert_equal(1, group.subjects.size)
+    assert_equal(subjects(:subject_1).id, group.subjects[0].id)
+  end
+
+  def test_questions
+    group = SubjectGroup.find( subject_groups(:sg_1).id )
+    assert_equal(3, group.questions.size)
+    assert_equal(questions(:q1).id, group.questions[0].id)
+    assert_equal(questions(:q2).id, group.questions[1].id)
+    assert_equal(questions(:q3).id, group.questions[2].id)
+  end
+
+  def test_resources
+    group = SubjectGroup.find( subject_groups(:sg_1).id )
+    assert_equal(1, group.resources.size)
+    assert_equal(resources(:resource_1).id, group.resources[0].id)
+  end
 end
