9d3b0541a5311461e3ba7ed01d31ed5d89a90fce
Peter Donald <peter@realityforge.org>
2006-02-11 05:05:17 +0000

    Start unit testing and cleaning up the question controller

--- app/controllers/teachers/question_controller.rb
+++ app/controllers/teachers/question_controller.rb
@@ -1,4 +1,7 @@
 class Teachers::QuestionController < Teachers::BaseController
+  verify :method => :get, :only => %w( list show )
+  verify :method => :post, :only => %w( add_resource remove_resource destroy )
+
   def list
     if params[:q]
       conditions = ['subject_group_id = ? AND content LIKE ?',
@@ -6,9 +9,9 @@ class Teachers::QuestionController < Teachers::BaseController
     else
       conditions = ['subject_group_id = ?', current_subject.subject_group_id]
     end
-    @question_pages, @questions = paginate( :question, 
-                                            :conditions => conditions,
-                                            :per_page => 10 )
+    @question_pages, @questions = paginate(:question, 
+                                           :conditions => conditions,
+                                           :per_page => 10)
   end
   
   def show
@@ -46,8 +49,8 @@ class Teachers::QuestionController < Teachers::BaseController
     @answers = @question.answers
   end  
   
-   def edit
-    @question = current_subject.subject_group.questions.find(params[:id])
+  def edit
+    @question = find_question(params[:id])
     if request.get?
       @answers = @question.answers
     elsif request.post?
@@ -97,37 +100,51 @@ class Teachers::QuestionController < Teachers::BaseController
   end
   
   def list_resources
-    if request.get?
-      @question = Question.find(params[:id])
-      @resources = Resource.find( :all, 
-            :conditions => ['subject_group_id = ? AND id NOT IN (SELECT resource_id FROM questions_resources WHERE question_id = ?)',
-                                                       current_subject.subject_group_id, @question.id] )
+    @question = find_question(params[:id])
+    if params[:q]
+      conditions = ['subject_group_id = ? AND name LIKE ? AND id NOT IN (SELECT resource_id FROM questions_resources WHERE question_id = ?)', 
+        current_subject.subject_group_id, "%#{params[:q]}%", @question.id]
+    else
+      conditions = ['subject_group_id = ? AND id NOT IN (SELECT resource_id FROM questions_resources WHERE question_id = ?)', 
+        current_subject.subject_group_id, @question.id]
     end
+    @resource_pages, @resources = paginate(:resource, 
+                                           :conditions => conditions,
+                                           :per_page => 10)
   end
   
   def add_resource
-    @question = Question.find(params[:question_id])
-    @question.resources << Resource.find(params[:resource_id])
+    question = find_question(params[:id])
+    question.resources << find_resource(params[:resource_id])
     flash[:notice] = 'Resource was successfully added to question.'
-    redirect_to( :action => 'list_resources', :id => @question )
+    redirect_to(:action => 'list_resources', :id => question.id)
   end
   
   def remove_resource
-    @resource = Resource.find(params[:resource_id])
-    @question = Question.find(params[:question_id])
-    @question.resources.delete(@resource)
+    resource = find_resource(params[:resource_id])
+    question = find_question(params[:id])
+    question.resources.delete(resource)
     flash[:notice] = 'Resource was successfully removed from question.'
-    redirect_to( :action => 'show', :id => @question )
+    redirect_to(:action => 'show', :id => question.id)
   end
   
   def destroy
-    @question = current_subject.subject_group.questions.find(params[:id])
-    if @question.quiz_items.length > 0
-      flash[:alert] = 'Question in quiz so cannot be destroyed'
+    question = find_question(params[:id])
+    if question.quiz_items.length > 0
+      flash[:alert] = 'Question in quiz so cannot be deleted.'
     else
       current_subject.subject_group.questions.find(params[:id]).destroy
+      flash[:notice] = 'Question was successfully deleted.'
     end
-    redirect_to( :action => 'list' )
+    redirect_to(:action => 'list', :q => params[:q], :page => params[:page])
   end
-  
+
+private
+  def find_question(question_id)
+    Question.find(question_id, :conditions => ['subject_group_id = ?', current_subject.subject_group_id])
+  end
+
+  def find_resource(resource_id)
+    Resource.find(resource_id, :conditions => ['subject_group_id = ?', current_subject.subject_group_id])
+  end
 end
---
+++ test/functional/teachers/question_controller_test.rb
@@ -0,0 +1,143 @@
+require File.dirname(__FILE__) + '/../../test_helper'
+require 'teachers/question_controller'
+
+#Re-raise errors caught by the controller.
+class Teachers::QuestionController; def rescue_action(e) raise e end; end
+
+module UploadedFile
+  attr_accessor :original_filename, :content_type
+end
+
+
+class Teachers::QuestionControllerTest < Test::Unit::TestCase
+  fixtures OrderedTables
+
+  def setup
+    @controller = Teachers::QuestionController.new
+    @request = ActionController::TestRequest.new
+    @response = ActionController::TestResponse.new
+  end
+
+  def test_list
+    get(:list,
+        {},
+        {:user_id => users(:lecturer_user).id, :role => :teacher, :subject_id => subjects(:subject_2).id})
+    assert_response(:success)
+    assert_template('list')
+    assert_valid_markup
+    assert_not_nil(assigns(:question_pages))
+    assert_not_nil(assigns(:questions))
+    assert_equal(3,assigns(:questions).length)
+    assert_equal(questions(:q4).id,assigns(:questions)[0].id)
+    assert_equal(questions(:q5).id,assigns(:questions)[1].id)
+    assert_equal(questions(:q6).id,assigns(:questions)[2].id)
+    assert_nil(flash[:alert])
+    assert_nil(flash[:notice])
+  end
+
+  def test_list_with_query
+    get(:list,
+        {:q => 'hay'},
+        {:user_id => users(:lecturer_user).id, :role => :teacher, :subject_id => subjects(:subject_2).id})
+    assert_response(:success)
+    assert_template('list')
+    assert_valid_markup
+    assert_not_nil(assigns(:question_pages))
+    assert_not_nil(assigns(:questions))
+    assert_equal(1,assigns(:questions).length)
+    assert_equal(questions(:q4).id,assigns(:questions)[0].id)
+    assert_nil(flash[:alert])
+    assert_nil(flash[:notice])
+  end
+
+  def test_show
+    get(:show,
+        {:id => questions(:q1).id},
+        {:user_id => users(:lecturer_user).id, :role => :teacher, :subject_id => subjects(:subject_1).id})
+    assert_response(:success)
+    assert_template('show')
+    assert_valid_markup
+    assert_not_nil(assigns(:question))
+    assert_nil(flash[:alert])
+    assert_nil(flash[:notice])
+  end
+
+
+
+
+
+
+  def test_list_resources
+    get(:list_resources,
+        {:id => questions(:q2).id},
+        {:user_id => users(:lecturer_user).id, :role => :teacher, :subject_id => subjects(:subject_1).id})
+    assert_response(:success)
+    assert_template('list_resources')
+    assert_valid_markup
+    assert_not_nil(assigns(:resource_pages))
+    assert_not_nil(assigns(:resources))
+    assert_equal(1,assigns(:resources).length)
+    assert_equal(resources(:resource_1).id,assigns(:resources)[0].id)
+    assert_nil(flash[:alert])
+    assert_nil(flash[:notice])
+  end
+
+  def test_list_resources_with_query
+    get(:list_resources,
+        {:id => questions(:q2).id, :q => 'lu'},
+        {:user_id => users(:lecturer_user).id, :role => :teacher, :subject_id => subjects(:subject_1).id})
+    assert_response(:success)
+    assert_template('list_resources')
+    assert_valid_markup
+    assert_not_nil(assigns(:resource_pages))
+    assert_not_nil(assigns(:resources))
+    assert_equal(1,assigns(:resources).length)
+    assert_equal(resources(:resource_1).id,assigns(:resources)[0].id)
+    assert_nil(flash[:alert])
+    assert_nil(flash[:notice])
+  end
+
+  def test_add_resource
+    assert_nil(questions(:q2).resources.find(:first, :conditions => ['id = ?', resources(:resource_1).id]))
+    post(:add_resource, 
+         {:id => questions(:q2).id, :resource_id => resources(:resource_1).id}, 
+         {:user_id => users(:lecturer_user).id, :role => :teacher, :subject_id => subjects(:subject_1).id})
+    assert_redirected_to(:action => 'list_resources', :id => questions(:q2).id)
+    assert_nil(flash[:alert])
+    assert_equal('Resource was successfully added to question.',flash[:notice])
+    assert_not_nil(questions(:q2).resources.find(:first, :conditions => ['id = ?', resources(:resource_1).id]))
+  end
+
+  def test_remove_resource
+    assert_not_nil(questions(:q1).resources.find(:first, :conditions => ['id = ?', resources(:resource_1).id]))
+    post(:remove_resource, 
+         {:id => questions(:q1).id, :resource_id => resources(:resource_1).id}, 
+         {:user_id => users(:lecturer_user).id, :role => :teacher, :subject_id => subjects(:subject_1).id})
+    assert_redirected_to(:action => 'show', :id => questions(:q1).id)
+    assert_nil(flash[:alert])
+    assert_equal('Resource was successfully removed from question.',flash[:notice])
+    assert_nil(questions(:q1).resources.find(:first, :conditions => ['id = ?', resources(:resource_1).id]))
+  end
+
+  def test_destroy_that_fails
+    assert_equal(true, Question.exists?(questions(:q1).id))
+    post(:destroy, 
+         {:id => questions(:q1).id, :q => 'q', :page => '1'}, 
+         {:user_id => users(:lecturer_user).id, :role => :teacher, :subject_id => subjects(:subject_1).id})
+    assert_redirected_to(:action => 'list', :q => 'q', :page => '1')
+    assert_nil(flash[:notice])
+    assert_equal('Question in quiz so cannot be deleted.',flash[:alert])
+    assert_equal(true, Question.exists?(questions(:q1).id))
+  end
+
+  def test_destroy 
+    assert_equal(true, Question.exists?(questions(:q10).id))
+    post(:destroy, 
+         {:id => questions(:q10).id, :q => 'q', :page => '1'}, 
+         {:user_id => users(:lecturer_user).id, :role => :teacher, :subject_id => subjects(:subject_1).id})
+    assert_redirected_to(:action => 'list', :q => 'q', :page => '1')
+    assert_nil(flash[:alert])
+    assert_equal('Question was successfully deleted.',flash[:notice])
+    assert_equal(false, Question.exists?(questions(:q10).id))
+  end
+end
