a7c417be3aeb1a8e910f47f912a5c25ada6d8273
Peter Donald <peter@realityforge.org>
2006-02-13 03:18:25 +0000

    Add unit tests

--- app/models/resource.rb
+++ app/models/resource.rb
@@ -1,18 +1,17 @@
 class Resource < ActiveRecord::Base
-  has_one( :resource_data, :dependent => true )
-  has_and_belongs_to_many( :questions )
-  validates_presence_of( :content_type, :name )
-  attr_accessible :description, :data
+  has_one(:resource_data, :dependent => true)
+  has_and_belongs_to_many(:questions)
+  validates_presence_of(:content_type, :name)
+  attr_accessible(:description, :data)
   
   def data=(data_field)
-    self.name = base_part_of(data_field.original_filename)
+    self.name = Resource.base_part_of(data_field.original_filename)
     self.content_type = data_field.content_type.chomp
     self.resource_data =
-        ResourceData.new( :resource_id => id, :data => data_field.read )
+        ResourceData.new(:resource_id => id, :data => data_field.read)
   end
   
-  def base_part_of( file_name )
-    name = File.basename(file_name)
-    name.gsub(/[^\w._-]/,'')
+  def self.base_part_of(file_name)
+    File.basename(file_name).gsub(/[^\w._-]/,'')
   end
 end
---
+++ test/unit/resource_test.rb
@@ -0,0 +1,29 @@
+require File.dirname(__FILE__) + '/../test_helper'
+
+
+class FakeFile
+  attr_accessor :original_filename, :content_type, :read
+  def initialize(original_filename, content_type, read)
+    @original_filename = original_filename
+    @content_type = content_type
+    @read = read
+  end
+end
+
+class ResourceTest < Test::Unit::TestCase
+  fixtures OrderedTables
+
+  def test_base_part_of
+    assert_equal('file.txt', Resource.base_part_of('/a/b/c/file.txt'))
+    assert_equal('file.txt', Resource.base_part_of('C:\a\file.txt'))
+  end
+
+  def test_data
+    resource = Resource.new(:description => 'description', :data => FakeFile.new('C:\a\file.txt','text/plain','ABCDEFGHIJKLMNOPQRSTUVWXYZ'))
+    assert_equal('description', resource.description)
+    assert_equal('file.txt', resource.name)
+    assert_equal('text/plain', resource.content_type)
+    assert_equal('ABCDEFGHIJKLMNOPQRSTUVWXYZ', resource.resource_data.data)
+  end
+
+end
