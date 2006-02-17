44755bf98aa203926af7a21f677b685779a77a74
Peter Donald <peter@realityforge.org>
2006-02-17 03:34:17 +0000

    Unit test more of question

--- app/models/question.rb
+++ app/models/question.rb
@@ -25,16 +25,16 @@ class Question < ActiveRecord::Base
   }.freeze
   
   def validate
-    if (question_type == Question::MultiOptionType || question_type == Question::SingleOptionType) && @choices
+    if (question_type == MultiOptionType || question_type == SingleOptionType) && @choices
       correct_count = 0
       choices.each_value do |choice|
         errors.add_to_base( 'All answers must have content of length 1 or more.' ) if (! choice[:content] || choice[:content].length == 0)
         correct_count += 1 if choice[:is_correct].to_s == 'true'
       end
-      errors.add_to_base( 'Must select single correct answer.' ) if (question_type == Question::SingleOptionType && correct_count != 1)
-    elsif question_type == Question::NumberType && @number_answer
+      errors.add_to_base( 'Must select single correct answer.' ) if (question_type == SingleOptionType && correct_count != 1)
+    elsif question_type == NumberType && @number_answer
       errors.add_to_base( 'Must specify a number answer.' ) if (! number_answer || !(number_answer.to_s =~ /^[+-]?\d+$/))
-    elsif question_type == Question::TextType && @text_answer
+    elsif question_type == TextType && @text_answer
       errors.add_to_base( 'Must have a answer of length of 1 or more.' ) if (! text_answer || text_answer.length == 0)
     end
   end
@@ -56,7 +56,7 @@ class Question < ActiveRecord::Base
   end
 
   def text_answer
-    @text_answer = (question_type == Question::TextType) ? answers[0].content : '' unless @text_answer
+    @text_answer = (question_type == TextType) ? answers[0].content : '' unless @text_answer
     @text_answer
   end
 
@@ -65,7 +65,7 @@ class Question < ActiveRecord::Base
   end
 
   def number_answer
-    @number_answer = (question_type == Question::NumberType) ? answers[0].content : '' unless @number_answer
+    @number_answer = (question_type == NumberType) ? answers[0].content : '' unless @number_answer
     @number_answer
   end
 
@@ -76,7 +76,7 @@ class Question < ActiveRecord::Base
   def choices
     if ! @choices
       @choices = {}
-      if question_type == Question::MultiOptionType || question_type == Question::SingleOptionType
+      if question_type == MultiOptionType || question_type == SingleOptionType
         self.answers.each do |answer|
           choices[answer.id.to_s] = {:content => answer.content, :position => answer.position, :is_correct => answer.is_correct}
         end
@@ -90,13 +90,13 @@ class Question < ActiveRecord::Base
   end
 
   def after_save
-    if question_type == Question::NumberType && @number_answer
+    if question_type == NumberType && @number_answer
       answers.clear
       Answer.create!(:question_id => id, :content => @number_answer, :position => 1, :is_correct => true)
-    elsif question_type == Question::TextType && @text_answer
+    elsif question_type == TextType && @text_answer
       answers.clear
       Answer.create!(:question_id => id, :content => @text_answer, :position => 1, :is_correct => true)
-    elsif (question_type == Question::MultiOptionType || question_type = Question::SingleOptionType) && @choices
+    elsif (question_type == MultiOptionType || question_type = SingleOptionType) && @choices
       answers.clear
       @choices.each_value do |choice|
         Answer.create!(:question_id => id, :content => choice[:content], :position => choice[:position], :is_correct => choice[:is_correct])
--- test/unit/question_test.rb
+++ test/unit/question_test.rb
@@ -45,4 +45,179 @@ class QuestionTest < Test::Unit::TestCase
   def check_choice(q,a)
     assert_equal({:is_correct => a.is_correct, :content => a.content, :position => a.position}, q.choices[a.id.to_s])
   end
+
+  def test_create_number_question
+    question = Question.new(:content => 'x', 
+                            :question_type => Question::NumberType, 
+                            :subject_group_id => subject_groups(:sg_1).id, 
+                            :corrected_at => Time.now,
+                            :text_format => TextFormatter::PlainFormat,
+                            :number_answer => '42')
+    assert_valid( question )
+
+    question.save!
+    question.reload
+
+    assert_equal('x', question.content)
+    assert_equal(Question::NumberType, question.question_type)
+    assert_equal(subject_groups(:sg_1).id, question.subject_group_id)
+    assert_not_nil(question.corrected_at)
+    assert_equal(TextFormatter::PlainFormat, question.text_format)
+    assert_equal(1, question.answers.length)
+    assert_equal('42', question.answers[0].content)
+    assert_equal(true, question.answers[0].is_correct?)
+    assert_equal(1, question.answers[0].position)
+  end
+
+  def test_invalid_create_number_question
+    question = Question.new(:content => 'x', 
+                            :question_type => Question::NumberType, 
+                            :subject_group_id => subject_groups(:sg_1).id, 
+                            :corrected_at => Time.now,
+                            :text_format => TextFormatter::PlainFormat,
+                            :number_answer => 'X')
+    assert_equal(false, question.valid?)
+    assert_equal('Must specify a number answer.', question.errors[:base])
+  end
+
+  def test_create_text_question
+    question = Question.new(:content => 'What am i?', 
+                            :question_type => Question::TextType, 
+                            :subject_group_id => subject_groups(:sg_1).id, 
+                            :corrected_at => Time.now,
+                            :text_format => TextFormatter::PlainFormat,
+                            :text_answer => '^ACE$')
+    assert_valid( question )
+
+    question.save!
+    question.reload
+
+    assert_equal('What am i?', question.content)
+    assert_equal(Question::TextType, question.question_type)
+    assert_equal(subject_groups(:sg_1).id, question.subject_group_id)
+    assert_not_nil(question.corrected_at)
+    assert_equal(TextFormatter::PlainFormat, question.text_format)
+    assert_equal(1, question.answers.length)
+    assert_equal('^ACE$', question.answers[0].content)
+    assert_equal(true, question.answers[0].is_correct?)
+    assert_equal(1, question.answers[0].position)
+  end
+
+  def test_invalid_create_text_question
+    question = Question.new(:content => 'x', 
+                            :question_type => Question::TextType, 
+                            :subject_group_id => subject_groups(:sg_1).id, 
+                            :corrected_at => Time.now,
+                            :text_format => TextFormatter::PlainFormat,
+                            :text_answer => '')
+    assert_equal(false, question.valid?)
+    assert_equal('Must have a answer of length of 1 or more.', question.errors[:base])
+  end
+
+  def test_create_single_choice_question
+    question = Question.new(:content => 'Should I have 1 or 2 scotch and cokes?', 
+                            :question_type => Question::SingleOptionType, 
+                            :subject_group_id => subject_groups(:sg_1).id, 
+                            :corrected_at => Time.now,
+                            :text_format => TextFormatter::PlainFormat)
+    question.choices = {
+      '1' => {:content => '1', :is_correct => false, :position => 1},
+      '2' => {:content => '2', :is_correct => true, :position => 2}
+    }
+    assert_valid( question )
+
+    question.save!
+    question.reload
+
+    assert_equal('Should I have 1 or 2 scotch and cokes?', question.content)
+    assert_equal(Question::SingleOptionType, question.question_type)
+    assert_equal(subject_groups(:sg_1).id, question.subject_group_id)
+    assert_not_nil(question.corrected_at)
+    assert_equal(TextFormatter::PlainFormat, question.text_format)
+    assert_equal(2, question.answers.length)
+    assert_equal('1', question.answers[0].content)
+    assert_equal(false, question.answers[0].is_correct?)
+    assert_equal(1, question.answers[0].position)
+    assert_equal('2', question.answers[1].content)
+    assert_equal(true, question.answers[1].is_correct?)
+    assert_equal(2, question.answers[1].position)
+  end
+
+
+  def test_create_single_choice_question
+    question = Question.new(:content => 'Should I have 1 or 2 scotch and cokes?', 
+                            :question_type => Question::MultiOptionType, 
+                            :subject_group_id => subject_groups(:sg_1).id, 
+                            :corrected_at => Time.now,
+                            :text_format => TextFormatter::PlainFormat)
+    question.choices = {
+      '1' => {:content => '1', :is_correct => true, :position => 1},
+      '2' => {:content => '2', :is_correct => true, :position => 2}
+    }
+    assert_valid( question )
+
+    question.save!
+    question.reload
+
+    assert_equal('Should I have 1 or 2 scotch and cokes?', question.content)
+    assert_equal(Question::MultiOptionType, question.question_type)
+    assert_equal(subject_groups(:sg_1).id, question.subject_group_id)
+    assert_not_nil(question.corrected_at)
+    assert_equal(TextFormatter::PlainFormat, question.text_format)
+    assert_equal(2, question.answers.length)
+    assert_equal('1', question.answers[0].content)
+    assert_equal(true, question.answers[0].is_correct?)
+    assert_equal(1, question.answers[0].position)
+    assert_equal('2', question.answers[1].content)
+    assert_equal(true, question.answers[1].is_correct?)
+    assert_equal(2, question.answers[1].position)
+  end
+
+  def test_invalid_create_choice_question_with_no_correct_answer
+    question = Question.new(:content => 'Should I have 1 or 2 scotch and cokes?', 
+                            :question_type => Question::SingleOptionType, 
+                            :subject_group_id => subject_groups(:sg_1).id, 
+                            :corrected_at => Time.now,
+                            :text_format => TextFormatter::PlainFormat)
+    question.choices = {
+      '1' => {:content => '1', :is_correct => false, :position => 1},
+      '2' => {:content => '2', :is_correct => false, :position => 2}
+    }
+    assert_equal(false, question.valid?)
+    assert_equal('Must select single correct answer.', question.errors[:base])
+  end
+
+  def test_invalid_create_choice_question_with_empty_content
+    question = Question.new(:content => 'Should I have 1 or 2 scotch and cokes?', 
+                            :question_type => Question::SingleOptionType, 
+                            :subject_group_id => subject_groups(:sg_1).id, 
+                            :corrected_at => Time.now,
+                            :text_format => TextFormatter::PlainFormat)
+    question.choices = {
+      '1' => {:content => '1', :is_correct => false, :position => 1},
+      '2' => {:content => '', :is_correct => true, :position => 2}
+    }
+    assert_equal(false, question.valid?)
+    assert_equal('All answers must have content of length 1 or more.', question.errors[:base])
+  end
+
+  def test_get_answers_on_non_randomized
+    4.times do |i|
+      ## Make sure non-randomised always returned in correct order
+      answer_ids1 = questions(:q4).get_answers.collect{|a| a.id}
+      answer_ids2 = questions(:q4).get_answers.collect{|a| a.id}
+      assert_equal(answer_ids1,answer_ids2)
+    end
+  end
+
+  def test_get_answers_on_randomized
+    count = 0
+    10.times do |i|
+      ## Make sure non-randomised always returned in correct order
+      answer_ids1 = questions(:q1).get_answers.collect{|a| a.id}
+      answer_ids2 = questions(:q1).get_answers.collect{|a| a.id}
+      count += 1 if (answer_ids1 != answer_ids2)
+    end
+    flunk('Answers not randomized') if (count == 10)
+  end
 end
