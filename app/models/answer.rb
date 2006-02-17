4d7295a2689ff305b95585552192362c216b453a
Nerida Ladner <spacecadetnerro@gmail.com>
2006-02-17 00:28:12 +0000

    i'm winning

--- app/models/answer.rb
+++ app/models/answer.rb
@@ -3,4 +3,16 @@ class Answer < ActiveRecord::Base
   belongs_to( :question )
   has_and_belongs_to_many( :quiz_responses, :uniq => true )
   acts_as_list( :scope => :question_id )
+  
+  def formatted_content
+    if self.question.text_format == Question::RedClothFormat
+      RedCloth.new( content ).to_html
+    elsif self.question.text_format == Question::BlueClothFormat
+      BlueCloth::new( content ).to_html
+    elsif self.question.text_format == Question::RubyPantsFormat
+      RubyPants.new( content ).to_html
+    else #assume PlainFormat
+      content
+    end
+  end
 end
