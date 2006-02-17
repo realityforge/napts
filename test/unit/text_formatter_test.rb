96575fff0f2ad393b3ba88825b955d6bd26be500
Nerida Ladner <spacecadetnerro@gmail.com>
2006-02-17 02:54:58 +0000

    wrote text_formatter_test

--- test/unit/text_formatter_test.rb
+++ test/unit/text_formatter_test.rb
@@ -3,6 +3,10 @@ require 'text_formatter'
 
 class TextFormatterTest < Test::Unit::TestCase
   def test_formatted_content
-    assert_equal('<h1>blah</h1>',TextFormatter.formatted_content(TextFormatter::RedClothFormat, '*'))
+    assert_equal('<h1>red</h1>',TextFormatter.formatted_content(TextFormatter::RedClothFormat, 'h1. red'))
+    assert_equal('<p><strong>blue</strong></p>', TextFormatter.formatted_content(TextFormatter::BlueClothFormat, '**blue**'))
+    assert_equal('&#8220;pants&#8221;', TextFormatter.formatted_content(TextFormatter::RubyPantsFormat, '"pants"'))
+    assert_equal( 'bob', TextFormatter.formatted_content(TextFormatter::PlainFormat, 'bob'))
+    assert_raise( RuntimeError ) {  TextFormatter.formatted_content('NeridaSmellsFunny', 'bob') }
   end
 end
