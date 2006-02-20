88976c04d5bc279336d4034566ac459dbc225375
Peter Donald <peter@realityforge.org>
2006-02-20 03:02:20 +0000

    Order subjects by name

--- app/models/subject.rb
+++ app/models/subject.rb
@@ -2,7 +2,7 @@ class Subject < ActiveRecord::Base
   # Can not modify subject group unless willing to clone all associated quizzes
   # and questions from old subject group into new subject group
   belongs_to( :subject_group )
-  has_many( :quizzes, :dependent => true )
+  has_many(:quizzes, :order => 'name', :dependent => true)
   has_and_belongs_to_many( :demonstrators, :class_name => 'User', :join_table => 'demonstrators', :uniq => true, :order => 'name' )
   has_and_belongs_to_many( :teachers, :class_name => 'User', :join_table => 'teachers', :uniq => true, :order => 'name' )
   has_and_belongs_to_many( :students, :class_name => 'User', :join_table => 'students', :uniq => true, :order => 'name' )
