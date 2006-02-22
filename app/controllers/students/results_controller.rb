dbdb11834c5416571cb01c766ad4f8d97484a943
Peter Donald <peter@realityforge.org>
2006-02-22 23:58:46 +0000

    Ensure that resources are accessible from results screen

--- app/controllers/students/results_controller.rb
+++ app/controllers/students/results_controller.rb
@@ -14,19 +14,39 @@ class Students::ResultsController < Students::BaseController
 				      
   end
   
-  #make sure it is the right user,  ~
-  #make sure publish_results is set to true  ~
-  #make sure they got that question wrong 
   def show_question
     @quiz_attempt = current_user.quiz_attempts.find(params[:id])
     if @quiz_attempt.quiz.publish_results?
-      @quiz_response = @quiz_attempt.quiz_responses.find( :first, :conditions => ['quiz_attempt_id = ? AND position = ?', @quiz_attempt.id, params[:position]])
-      if @quiz_response.correct?
-        flash[:alert] = 'This question was answered correctly'
-      end
+      @quiz_response = @quiz_attempt.quiz_responses.find(:first, 
+                                                         :conditions => ['quiz_attempt_id = ? AND position = ?', @quiz_attempt.id, params[:position]])
+      @resource_base = {:action => 'resource', :id => @quiz_attempt.quiz_id, :position => @quiz_response.position}
+      flash[:alert] = 'This question was answered correctly' if @quiz_response.correct?
     else
       flash[:alert] = 'Results are not currently enabled'
       redirect_to( :action => 'show', :id => @quiz_attempt )
     end
+  end
+
+
+  def resource
+    resource = Resource.find(:first,
+                             :select => 'resources.*',
+                             :conditions => ['quizzes.publish_results = ? AND quizzes.id = ? AND quiz_attempts.end_time IS NOT NULL AND quiz_attempts.user_id = ? AND quiz_responses.position = ? AND resources.name = ?', 
+                               true, params[:id], current_user.id, params[:position], params[:name] ],
+                             :joins => 
+                               'LEFT OUTER JOIN questions_resources ON resources.id = questions_resources.resource_id ' +
+                               'LEFT OUTER JOIN questions ON questions_resources.question_id = questions.id ' +
+                               'LEFT OUTER JOIN quiz_responses ON questions.id = quiz_responses.question_id ' +
+                               'LEFT OUTER JOIN quiz_attempts ON quiz_responses.quiz_attempt_id = quiz_attempts.id ' +
+                               'LEFT OUTER JOIN quizzes ON quiz_attempts.quiz_id = quizzes.id'
+                             )
+    raise ActiveRecord::RecordNotFound, "Couldn't find Resource named #{params[:name]} for Quiz.id = #{params[:id]} AND position = #{params[:position]}" unless resource
+
+    disposition = (params[:disposition] == 'download') ? 'download' : 'inline'
+    
+    send_data(resource.resource_data.data, 
+              :filename => resource.name,
+	      :type => resource.content_type,
+	      :disposition => disposition )
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 ActionController::Routing::Routes.draw do |map|
   map.connect '', :controller => 'login', :action => 'login'
-  # Install the default route as the lowest priority.
-  map.connect ':controller/:action/:id'
   map.connect '/students/preview/resource/:id/:position/:name', :controller => 'students/preview', :action => 'resource'
   map.connect '/students/quiz_attempt/resource/:id/:position/:name', :controller => 'students/quiz_attempt', :action => 'resource'
+  map.connect '/students/results/resource/:id/:position/:name', :controller => 'students/results', :action => 'resource'
+
+  # Install the default route as the lowest priority.
+  map.connect ':controller/:action/:id'
 end
