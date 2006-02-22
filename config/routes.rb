8ffa0556868f5d3ec8fd934ea675b3450e19969b
Peter Donald <peter@realityforge.org>
2006-02-22 05:22:36 +0000

    Add in route for viewing resources associated with preview

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,5 @@ ActionController::Routing::Routes.draw do |map|
   map.connect '', :controller => 'login', :action => 'login'
   # Install the default route as the lowest priority.
   map.connect ':controller/:action/:id'
+  map.connect '/students/preview/resource/:id/:position/:name', :controller => 'students/preview', :action => 'resource'
 end
