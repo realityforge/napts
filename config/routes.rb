eae5c8819b90cb27466053706b8a7c36c537039f
Peter Donald <peter@realityforge.org>
2006-02-22 23:26:03 +0000

    Expose resources in quiz_attempt_controller

--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,5 @@ ActionController::Routing::Routes.draw do |map|
   # Install the default route as the lowest priority.
   map.connect ':controller/:action/:id'
   map.connect '/students/preview/resource/:id/:position/:name', :controller => 'students/preview', :action => 'resource'
+  map.connect '/students/quiz_attempt/resource/:id/:position/:name', :controller => 'students/quiz_attempt', :action => 'resource'
 end
