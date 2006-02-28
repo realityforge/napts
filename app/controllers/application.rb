f131a9e6311989c045b9ce52eca302ad45b60fef
Peter Donald <peter@realityforge.org>
2006-02-28 05:34:20 +0000

    Remove uneeded helper declaration

--- app/controllers/application.rb
+++ app/controllers/application.rb
@@ -1,7 +1,6 @@
 # Filters added to this controller will be run for all controllers in the application.
 # Likewise, all the methods added will be available for all controllers.
 class ApplicationController < ActionController::Base
-  helper :view_debug
   helper :menu
   helper :auth
   include AuthHelper
