6fd13147765c6c99b0d68bb13fd9ce65891f9302
Peter Donald <peter@realityforge.org>
2006-03-08 02:05:05 +0000

    Simplify the unit test

--- test/functional/css_test.rb
+++ test/functional/css_test.rb
@@ -1,11 +1,5 @@
 require File.dirname(__FILE__) + '/../test_helper'
 
 class CssTest < Test::Unit::TestCase
-  def test_layout_css
-    assert_valid_css(File.open("#{RAILS_ROOT}/public/stylesheets/layout.css",'rb').read)
-  end
-
-  def test_standard_css
-    assert_valid_css(File.open("#{RAILS_ROOT}/public/stylesheets/standard.css",'rb').read)
-  end
+  assert_valid_css 'layout', 'standard'
 end
